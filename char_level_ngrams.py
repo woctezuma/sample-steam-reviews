5c106008d279bf230ef7b86ce1a6c9693e2c0234
Wok <woctezuma@users.noreply.github.com>
2019-03-07 14:25:56 +0100

    Print info regarding corpus size (with and without punctuation)

--- char_level_ngrams.py
+++ char_level_ngrams.py
@@ -4,9 +4,11 @@
 # -   https://gist.github.com/joe-sullivan/fe8f486fc23ee51db14e56a7a2141c59
 # -   https://nbviewer.jupyter.org/gist/yoavg/d76121dfde2618422139
 
+import string
 from collections import *
 from random import random
 
+from download_review_data import get_artifact_app_id
 from export_review_data import get_output_file_name
 
 
@@ -47,7 +49,26 @@ def generate_text(lm, order, nletters=1000):
     return ''.join(out)
 
 
+def print_corpus_info(app_id=None):
+    fname = get_output_file_name(app_id)
+    with open(fname, 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    print('Corpus size: {} characters (with puncutation).'.format(len(data)))
+
+    # Reference: https://gist.github.com/maxim5/c35ef2238ae708ccb0e55624e9e0252b#gistcomment-2839110
+    translator = str.maketrans('', '', string.punctuation)
+    data = data.translate(translator)
+
+    print('Corpus size: {} characters (without puncutation).'.format(len(data)))
+
+    return
+
+
 if __name__ == '__main__':
+    app_id = get_artifact_app_id()
+    print_corpus_info(app_id)
+
     order = 20
-    lm = train_char_lm(get_output_file_name(), order=order)
+    lm = train_char_lm(get_output_file_name(app_id), order=order)
     print(generate_text(lm, order))
